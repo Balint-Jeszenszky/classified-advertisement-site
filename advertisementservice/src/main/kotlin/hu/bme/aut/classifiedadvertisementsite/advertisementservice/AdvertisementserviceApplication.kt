a9bf1410a8505308a195406de3e6dce36a9d495a
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-03-28 20:55:38 +0200

    advertisement microservice init

---
+++ advertisementservice/src/main/kotlin/hu/bme/aut/classifiedadvertisementsite/advertisementservice/AdvertisementserviceApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.classifiedadvertisementsite.advertisementservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvertisementserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdvertisementserviceApplication>(*args)
+}
---
+++ advertisementservice/src/test/kotlin/hu/bme/aut/classifiedadvertisementsite/advertisementservice/AdvertisementserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.classifiedadvertisementsite.advertisementservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvertisementserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
