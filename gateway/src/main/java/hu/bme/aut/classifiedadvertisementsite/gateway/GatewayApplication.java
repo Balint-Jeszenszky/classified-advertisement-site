11841dd7d39d97ca6484b0c960f62249c5ceb9aa
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-03-30 16:36:22 +0200

    gateway routing refactored and advertisement service added to it

--- gateway/src/main/java/hu/bme/aut/classifiedadvertisementsite/gateway/GatewayApplication.java
+++ gateway/src/main/java/hu/bme/aut/classifiedadvertisementsite/gateway/GatewayApplication.java
@@ -2,22 +2,9 @@ package hu.bme.aut.classifiedadvertisementsite.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/api/user/**")
-						.filters(f -> f.rewritePath("/api/user/", "/api/user/external/"))
-						.uri("http://localhost:8081"))
-				.build();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
---
+++ gateway/src/main/java/hu/bme/aut/classifiedadvertisementsite/gateway/Router.java
@@ -0,0 +1,30 @@
+package hu.bme.aut.classifiedadvertisementsite.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Router {
+
+    @Value("${gateway.services.userservice}")
+    private String userServiceUri;
+    @Value("${gateway.services.advertisementservice}")
+    private String advertisementServiceUri;
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/user/**")
+                        .filters(f -> f.rewritePath("/api/user/", "/api/user/external/"))
+                        .uri(userServiceUri))
+                .route(p -> p
+                        .path("/api/advertisement/**")
+                        .filters(f -> f.rewritePath("/api/advertisement/", "/api/advertisement/external/"))
+                        .uri(advertisementServiceUri))
+                .build();
+    }
+}
