56443b9351e99ea3c5cf83d9455c67105e13fa56
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2023-04-09 13:30:22 +0200

    delete advertisements

--- classified-advertisement-site-frontend/src/app/advertisement/details/details.component.ts
+++ classified-advertisement-site-frontend/src/app/advertisement/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AdvertisementResponse, AdvertisementService } from 'src/app/openapi/advertisementservice';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdvertisementResponse, AdvertisementService, CategoryResponse, CategoryService } from 'src/app/openapi/advertisementservice';
+import { LoggedInUserService } from 'src/app/service/logged-in-user.service';
+import { Role } from 'src/app/service/types';
 
 @Component({
   selector: 'app-details',
@@ -10,18 +12,53 @@ import { AdvertisementResponse, AdvertisementService } from 'src/app/openapi/adv
 export class DetailsComponent implements OnInit {
   id?: number;
   advertisement?: AdvertisementResponse;
+  category: CategoryResponse[] = [];
+  admin: boolean = false;
+  owner: boolean = false;
 
   constructor(
-    private route: ActivatedRoute,
-    private advertisementService: AdvertisementService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly advertisementService: AdvertisementService,
+    private readonly categoryService: CategoryService,
+    private readonly loggedInUserService: LoggedInUserService,
   ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = +params['id'];
-      this.advertisementService.getAdvertisementId(this.id).subscribe(res => {
-        this.advertisement = res;
+      this.advertisementService.getAdvertisementId(this.id).subscribe(ad => {
+        this.advertisement = ad;
+        this.categoryService.getCategories().subscribe({
+          next: cat => this.setCategory(cat, ad.categoryId),
+        });
+        this.loggedInUserService.user.subscribe(user => {
+          if (user) {
+            this.owner = user.id === ad.advertiserId;
+            this.admin = user.roles.includes(Role.ROLE_ADMIN);
+          }
+        })
       });
     });
+  }
+
+  onDelete() {
+    if (this.id && confirm("Delete advertisement?")) {
+      this.advertisementService.deleteAdvertisementId(this.id).subscribe({
+        next: () => this.router.navigate(['/category/', this.advertisement?.categoryId]),
+      });
+    }
+  }
+
+  private setCategory(categories: CategoryResponse[], id?: number) {
+    let parentCategoryId: number | undefined = id;
+    this.category = [];
+    do {
+      const currentCategory = categories.find(e => e.id === parentCategoryId);
+      if (currentCategory) {
+        this.category.unshift(currentCategory);
+        parentCategoryId = currentCategory.parentCategoryId;
+      }
+    } while (parentCategoryId);
   }
 }
