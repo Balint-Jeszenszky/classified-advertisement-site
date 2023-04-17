38b4b48511857bc06c088f35932f25ffa0b5716f
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-04-17 18:19:10 +0200

    image processing microservice init

---
+++ imageprocessingservice/src/main/java/hu/bme/aut/classifiedadvertisementsite/imageprocessingservice/ImageprocessingserviceApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.classifiedadvertisementsite.imageprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprocessingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageprocessingserviceApplication.class, args);
+	}
+
+}
---
+++ imageprocessingservice/src/test/java/hu/bme/aut/classifiedadvertisementsite/imageprocessingservice/ImageprocessingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.classifiedadvertisementsite.imageprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
