c67e454a2a5ebfde6a80dd4ec5778ffe4bc16045
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-04-27 19:32:22 +0200

    validation added to DTOs

--- web-scraping-service/src/scraper/dto/SiteRequest.dto.ts
+++ web-scraping-service/src/scraper/dto/SiteRequest.dto.ts
@@ -1,19 +1,46 @@
-export class SiteRequest {
-  categoryId: number;
-  name: string;
-  url: string;
-  selector: RootSelector;
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, ValidateNested } from "class-validator";
+
+export class Selector {
+  @IsNotEmpty()
+  selector: string;
+
+  @IsNotEmpty()
+  property: string;
 }
 
 export class RootSelector {
+  @IsNotEmpty()
   base: string;
+  
+  @ValidateNested({ each: true })
+  @Type(() => Selector)
   image: Selector;
+  
+  @ValidateNested({ each: true })
+  @Type(() => Selector)
   price: Selector;
+  
+  @ValidateNested({ each: true })
+  @Type(() => Selector)
   title: Selector;
+  
+  @ValidateNested({ each: true })
+  @Type(() => Selector)
   url: Selector;
 }
 
-export class Selector {
-  selector: string;
-  property: string;
+export class SiteRequest {
+  @IsNumber()
+  categoryId: number;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  url: string;
+  
+  @ValidateNested({ each: true })
+  @Type(() => Selector)
+  selector: RootSelector;
 }
