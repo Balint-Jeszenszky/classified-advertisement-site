89b4a59b6f0f26e327082e0b0b366bf95db824fb
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-04-27 19:41:38 +0200

    .env config

--- web-scraping-service/src/app.module.ts
+++ web-scraping-service/src/app.module.ts
@@ -5,12 +5,16 @@ import { ScraperModule } from './scraper/scraper.module';
 import { RolesGuard } from './auth/role.guard';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ScraperModule,
     MongooseModule.forRoot('mongodb://localhost/scraper'),
     ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [],
   providers: [
