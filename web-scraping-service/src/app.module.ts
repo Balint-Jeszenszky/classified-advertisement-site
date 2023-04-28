1b8742dd594bc6377460bfe0c6913dea03290f11
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2023-04-28 23:01:50 +0200

    Web scraper microservice dockerized

--- web-scraping-service/src/app.module.ts
+++ web-scraping-service/src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ScraperModule,
-    MongooseModule.forRoot('mongodb://localhost/scraper'),
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_URL}/scraper`),
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
