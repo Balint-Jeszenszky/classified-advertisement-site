d22ca991c73d3c5d6700dc047cfa959357304d27
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-09-08 17:08:54 +0200

    email cnfiguration

--- notificationservice/src/notification/notification.module.ts
+++ notificationservice/src/notification/notification.module.ts
@@ -2,8 +2,36 @@ import { Module } from '@nestjs/common';
 import { MessageQueueController } from './message-queue.controller';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT),
+        tls: {
+          ciphers: 'SSLv3',
+        },
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <user@outlook.com>', // outgoing email ID
+      },
+      template: {
+        dir: process.cwd() + '/template/',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [MessageQueueController, NotificationController],
   providers: [NotificationService]
 })
--- notificationservice/src/notification/notification.service.ts
+++ notificationservice/src/notification/notification.service.ts
@@ -1,10 +1,13 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { setVapidDetails, sendNotification } from 'web-push';
 
 @Injectable()
 export class NotificationService {
 
-  constructor() {
+  constructor(
+    private readonly mailerService: MailerService,
+  ) {
     setVapidDetails(
       `mailto:${process.env.ADMIN_EMAIL}`,
       process.env.VAPID_PUBLIC_KEY,
@@ -18,5 +21,24 @@ export class NotificationService {
 
   getPublicVapidKey(): string {
     return process.env.VAPID_PUBLIC_KEY;
+  }
+
+  sendEmail() {
+    this.mailerService.sendMail({
+      to: 'user@gmail.com', // List of receivers email address
+      from: 'user@outlook.com', // Senders email address
+      subject: 'Testing Nest Mailermodule with template ✔',
+      template: 'index', // The `.pug` or `.hbs` extension is appended automatically.
+      context: {  // Data to be sent to template engine.
+        code: 'cf1a3f828287',
+        username: 'john doe',
+      },
+    })
+    .then((success) => {
+      console.log(success);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 }
