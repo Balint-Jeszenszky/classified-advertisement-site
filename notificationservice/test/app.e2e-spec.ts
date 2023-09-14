f8528dfd142e47e2d125096c8bcd11556858dcad
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-09-14 21:35:41 +0200

    notification service tests

--- notificationservice/test/app.e2e-spec.ts
+++ notificationservice/test/app.e2e-spec.ts
@@ -14,4 +14,43 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+
+  it('/notification/publicVapidKey (GET) should return vapid key', () => {
+    return request(app.getHttpServer())
+      .get('/notification/publicVapidKey')
+      .set('x-user-data', getUserAuthHeader())
+      .expect(200)
+      .expect({
+        publicVapidKey: process.env.VAPID_PUBLIC_KEY,
+      });
+  });
+
+  it('/notification/publicVapidKey (GET) should return error without authenticatuon header', () => {
+    return request(app.getHttpServer())
+      .get('/notification/publicVapidKey')
+      .expect(401);
+  });
+
+  it('/notification/pushSubscription (POST) should accept payload', () => {
+    return request(app.getHttpServer())
+      .post('/notification/pushSubscription')
+      .set('x-user-data', getUserAuthHeader())
+      .send({ endpoint: 'endpoint', keys: { p256dh: 'p256dh', auth: 'auth' }})
+      .expect(201);
+  });
+
+  it('/notification/pushSubscription (POST) should return error without authenticatuon header', () => {
+    return request(app.getHttpServer())
+      .post('/notification/pushSubscription')
+      .send({ endpoint: 'endpoint', keys: { p256dh: 'p256dh', auth: 'auth' }})
+      .expect(401);
+  });
 });
+
+function getUserAuthHeader() {
+  return 'eyJ1c2VybmFtZSI6InVzZXIiLCJpZCI6MiwiZW1haWwiOiJ1c2VyQHVzZXIubG9jYWwiLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0=';
+}
+
+function getAdminAuthHeader() {
+  return 'eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOjEsImVtYWlsIjoiYWRtaW5AYWRtaW4ubG9jYWwiLCJyb2xlcyI6WyJST0xFX0FETUlOIiwiUk9MRV9VU0VSIl19';
+}
