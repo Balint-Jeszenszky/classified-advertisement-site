6b8891253fd6eb9bcda1d68365b2c21c04848d5c
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2023-10-14 23:35:20 +0200

    bid service created

---
+++ bidservice/src/main/kotlin/hu/bme/aut/classifiedadvertisementsite/bidservice/BidserviceApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.classifiedadvertisementsite.bidservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BidserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BidserviceApplication>(*args)
+}
---
+++ bidservice/src/test/kotlin/hu/bme/aut/classifiedadvertisementsite/bidservice/BidserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.classifiedadvertisementsite.bidservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BidserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
