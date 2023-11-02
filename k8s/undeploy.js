97e10ddb331f55398cdaa35a7a98c185b57399aa
Balint-Jeszenszky <kisjeszeni1234@gmail.com>
2023-11-02 19:37:43 +0100

    kubernetes persistent volumes

--- k8s/undeploy.js
+++ k8s/undeploy.js
@@ -3,6 +3,7 @@ const { execSync } = require('child_process');
 
 const CONFIG = '-config.yaml';
 const SECRET = '-secret.yaml';
+const NAMESPACE = 'adsite';
 
 const dirs = readdirSync('./', { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
@@ -13,23 +14,7 @@ dirs.forEach(dir => {
     .filter(dirent => !dirent.isDirectory())
     .map(dirent => dirent.name)
     .filter(file => !file.endsWith(CONFIG) && !file.endsWith(SECRET))
-    .forEach(file => console.log(execSync(`kubectl delete deployment ${file.split('.')[0]}-deployment`).toString()));
-});
-
-dirs.forEach(dir => {
-  readdirSync(`./${dir}`, { withFileTypes: true })
-    .filter(dirent => !dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .filter(file => !file.endsWith(CONFIG) && !file.endsWith(SECRET))
-    .forEach(file => console.log(execSync(`kubectl delete service ${file.split('.')[0]}-service`).toString()));
-});
-
-dirs.forEach(dir => {
-  readdirSync(`./${dir}`, { withFileTypes: true })
-    .filter(dirent => !dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .filter(file => file.endsWith(SECRET))
-    .forEach(file => console.log(execSync(`kubectl delete secret ${file.split('.')[0]}`).toString()));
+    .forEach(file => console.log(execSync(`kubectl -n ${NAMESPACE} delete -f ${dir}/${file}`).toString()));
 });
 
 dirs.forEach(dir => {
@@ -37,5 +22,15 @@ dirs.forEach(dir => {
     .filter(dirent => !dirent.isDirectory())
     .map(dirent => dirent.name)
     .filter(file => file.endsWith(CONFIG))
-    .forEach(file => console.log(execSync(`kubectl delete configmap ${file.split('.')[0]}`).toString()));
+    .forEach(file => console.log(execSync(`kubectl -n ${NAMESPACE} delete -f ${dir}/${file}`).toString()));
 });
+
+dirs.forEach(dir => {
+  readdirSync(`./${dir}`, { withFileTypes: true })
+    .filter(dirent => !dirent.isDirectory())
+    .map(dirent => dirent.name)
+    .filter(file => file.endsWith(SECRET))
+    .forEach(file => console.log(execSync(`kubectl -n ${NAMESPACE} delete -f ${dir}/${file}`).toString()));
+});
+
+console.log(execSync(`kubectl delete ns ${NAMESPACE}`).toString());
