2cd7b01bb93a7421e42739f4fc19031f9ae5038d
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2023-12-03 17:41:34 +0100

    notification service tests

--- notificationservice/src/notification/message-queue.controller.spec.ts
+++ notificationservice/src/notification/message-queue.controller.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { generateVAPIDKeys } from 'web-push'
 import { MessageQueueController } from './message-queue.controller';
+import { NotificationService } from './notification.service';
+import { PushSubscription } from './schema/push-subscription.model';
 
 describe('MessageQueueController', () => {
   let controller: MessageQueueController;
+  
+  beforeAll(() => {
+    const { publicKey, privateKey } = generateVAPIDKeys();
+    process.env.VAPID_PUBLIC_KEY = publicKey;
+    process.env.VAPID_PRIVATE_KEY = privateKey;
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        NotificationService,
+        { 
+          provide: getModelToken(PushSubscription.name),
+          useValue: Model,
+        },
+      ],
       controllers: [MessageQueueController],
+    }).useMocker((token) => {
+      const moduleMocker = new ModuleMocker(global);
+      const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+      const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+      return new Mock();
     }).compile();
 
     controller = module.get<MessageQueueController>(MessageQueueController);
